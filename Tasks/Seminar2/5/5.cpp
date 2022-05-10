8cb9bf5ea62fea5685d32fcac386a80e671dcad2
Vadim <vadimosipovcng@gmail.com>
2022-05-10 09:30:32 +0300

    add Sem2 task5

---
+++ Tasks/Seminar2/5/5.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+int factorial(int i);
+
+int main()
+{
+    double num, fact;
+
+    cout << "Enter number(0-20): ";
+    cin >> num;
+
+    while (num < 0 || num >= 20)
+    {
+        cout << "Enter another number(0-20): ";
+        cin >> num;
+    }
+
+    fact = factorial(num);
+    cout << "Factorial of " << num << " is " << fact << endl;
+}
+
+int factorial(int i)
+{
+    if (i == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return i * factorial(i - 1);
+    }
+
+}
